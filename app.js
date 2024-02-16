1ca794c005748f6d3d68f6d3b169815d68cf2159
Ann Catherine Jose <ann.jose@gmail.com>
2024-02-16 12:36:21 -0800

    Fixed the error handling so that it bubbles up the login error from the source all the way to the caller with proper Promise reject/try-catch. Also updated the caller sites (the API, the web page and the script) to handle the error correctly.

--- app.js
+++ app.js
@@ -15,9 +15,15 @@ app.get('/', (req, res) => {
 
 app.post('/api/get-monarch-accounts', async (req, res) => {
 
-    const accounts = await getMonarchAccounts();
-    console.log(accounts);
-    res.send(accounts);
+    console.log(`Received request to API route: /api/get-monarch-accounts`); 
+    try {
+        const accounts = await getMonarchAccounts();
+        console.log(accounts);
+        res.send(accounts);
+    } catch (error) {
+        console.log(`getMonarchAccounts() returned error ${error}`);
+        res.status(400).send({errorDetail: error.message});
+    }
 });
 
 app.listen(port, () => {
